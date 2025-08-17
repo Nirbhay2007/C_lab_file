b100fc67170c8a81a4809b5458a67e36aca985e4
Nirbhay Garg <161045184+Nirbhay2007@users.noreply.github.com>
2025-08-17 12:39:35 +0530

    Update Basic _info.c

--- Basic _info.c
+++ Basic _info.c
@@ -4,6 +4,6 @@ int main() {
     printf("Name :- Nirbhay Garg\n");
     printf("Sap ID :- 590022827\n");
     printf("email :- nirbhaygarg11@gmail.com\n");
-    printf("Contact No :- 8195010059\n");
+    printf("Contact No :- 8*******59\n");
     return 0;
 }
